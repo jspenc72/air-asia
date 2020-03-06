b5c1faff63a3b16c7c60fc3230a71d4c8e7bc7e6
jspenc72 <jspenc72@gmail.com>
2020-03-05 21:54:20 -0700

    Generate Pages for Assignment 2
    - login
    - card-update
    - card-list
    - card-details
    - card-add

--- air-asia-client/src/app/app.module.ts
+++ air-asia-client/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { CardListComponent } from './card-list/card-list.component';
+import { CardDetailsComponent } from './card-details/card-details.component';
+import { CardAddComponent } from './card-add/card-add.component';
+import { CardUpdateComponent } from './card-update/card-update.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CardListComponent,
+    CardDetailsComponent,
+    CardAddComponent,
+    CardUpdateComponent
   ],
   imports: [
     BrowserModule,
