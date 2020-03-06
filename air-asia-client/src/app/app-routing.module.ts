b9f27f68ee6dc64240837a2e6cf7b320874f68a6
u0545288 <u0545288@utah.edu>
2020-03-05 22:48:50 -0700

    Added Login Component
    - Username
    -Password
    -Login Button
      > Forwards to card-list page

--- air-asia-client/src/app/app-routing.module.ts
+++ air-asia-client/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import  { LoginComponent }  from './login/login.component';
+import  { CardListComponent }  from './card-list/card-list.component';
 
-
-const routes: Routes = [{ path: 'login', component: LoginComponent }
+const routes: Routes = [{ path: 'login', component: LoginComponent }, 
+{ path: 'card-list', component: CardListComponent }
 ];
 
 @NgModule({
