84cbf7bbc4750f466dce98b1005de0d618c1da96
jspenc72 <jspenc72@gmail.com>
2020-03-26 22:22:14 -0600

    Add resource routes for entities
    User
    Redemption
    GiftCard
    Account

--- server/air-asia-server/routes/api.php
+++ server/air-asia-server/routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('user', 'UserController');
+Route::resource('account', 'AccountController');
+Route::resource('redemption', 'RedemptionController');
+Route::resource('gift-card', 'GiftCardController');
