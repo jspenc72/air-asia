fa0c78bc7c36c122303d2a59808e69190d7b4ffe
jspenc72 <jspenc72@gmail.com>
2020-03-27 16:40:40 -0600

    Added seeders.

--- air-asia-server/database/seeds/UserSeeder.php
+++ air-asia-server/database/seeds/UserSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Faker\Factory as Faker;
 class UserSeeder extends Seeder
 {
     /**
@@ -11,6 +11,17 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker::create();
+        for ($x = 0; $x <= 5; $x++) {
+            DB::table('users')->insert([
+                'username' => Str::random(10),
+                'firstName' => $faker->firstName,
+                'lastName' => $faker->lastName,
+                'role' => 'user',
+                'email' => Str::random(10).'@gmail.com',
+                'password' => Hash::make('password'),
+            ]);            
+        } 
+
     }
 }
