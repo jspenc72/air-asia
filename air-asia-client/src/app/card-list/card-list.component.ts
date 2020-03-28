5eb7ab03b4492993e02c68aaa74a665b0f657c05
jspenc72 <jspenc72@gmail.com>
2020-03-28 09:10:38 -0600

    Finish delete gift card functionality.

--- air-asia-client/src/app/card-list/card-list.component.ts
+++ air-asia-client/src/app/card-list/card-list.component.ts
@@ -13,9 +13,7 @@ export class CardListComponent implements OnInit {
   state: any
   constructor(private giftCardService: GiftCardService, private router: Router) { 
     this.state = router.routerState;
-    this.items = [{ cardName: "card1", type: "", points: 100, image: "https://d2e70e9yced57e.cloudfront.net/wallethub/posts/68808/best-gift-cards.png"}, 
-    { cardName: "card2", type: "", points: 100, image: "https://gawdamedia.com/wp-content/uploads/2019/07/generic-gift-card.jpeg"}, 
-    { cardName: "card3", type: "", points: 100, image: "https://cdn.shopify.com/s/files/1/0963/1508/products/GenericGiftCard1_1024x1024.jpg?v=1575933245"}]
+    this.items = []
   }
   
   displayCardDetail(item): void {
@@ -36,20 +34,4 @@ export class CardListComponent implements OnInit {
   ngOnInit(): void {
     this.reloadGiftCards()
   }
-
-  ngDoCheck(): void {
-    console.log("ngDoCheck");
-  }
-
-  ngAfterContentChecked(): void {
-    console.log("ngAfterContentChecked");
-  }
-
-  ngAfterViewChecked(): void {
-    console.log("ngAfterViewChecked");
-    
-  }
-
-
-
 }
